e880af1e6e2bf35123025b2b2ea7e8c84cbbf3c8
Matthew D. Alexander <mda54092@gmail.com>
2016-10-19 14:36:54 -0500

    working vectorizor, but bad word count

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,35 +41,54 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            email_txt = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+            reps = ["sara", "shackleton", "chris", "germani"]
+            for rep in reps:
+                 email_txt_removed = email_txt.replace(rep, "")
+
 
             ### append the text to word_data
-
+            word_data.append(email_txt_removed)
+            
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if name == "sara":
+                from_data.append("0")
+            elif name == "chris":
+                from_data.append("1")
 
 
             email.close()
 
 print "emails processed"
+print word_data[152]
+print(from_data)
+
 from_sara.close()
 from_chris.close()
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
-
-
-
 ### in Part 4, do TfIdf vectorization here
+
+from sklearn.feature_extraction.text import CountVectorizer
+# from sklean.feature_extraction import text
+
+vectorizer = CountVectorizer(stop_words='english')
+vectorizer.fit_transform(word_data)
+vocab_list = vectorizer.get_feature_names()
+print len(vocab_list)
+
+
+
 
 

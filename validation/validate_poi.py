5d84772b181a33ebd5623c5252e19626b0de05ce
Matthew D. Alexander <mda54092@gmail.com>
2016-10-24 13:15:26 -0500

    finished validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,6 +14,8 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import train_test_split
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -25,8 +27,19 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
-
-
 ### it's all yours from here forward!  
 
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
+
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+
+
+pred = clf.predict(features_test)
+
+
+acc = accuracy_score(labels_test, pred)
+print acc
 
